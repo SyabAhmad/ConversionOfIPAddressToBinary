0354bc98bc8db999dd62aad48f8e957c8a5ff2ab
DEVELoper <syabblogger@gmail.com>
2022-12-21 21:03:00 +0500

    Initial commit

---
+++ app/src/main/java/com/example/conversionofipaddresstobinary/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.conversionofipaddresstobinary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    String firstValueOfIP;
+
+        TextView firstValTextView = (TextView) findViewById(R.id.value1);
+        TextView secondValTextView = (TextView) findViewById(R.id.value2);
+        TextView thirdValTextView = (TextView) findViewById(R.id.value3);
+        TextView fourthValTextView = (TextView) findViewById(R.id.value4);
+
+    }
+}
