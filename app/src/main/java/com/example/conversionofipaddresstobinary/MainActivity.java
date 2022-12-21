f3a9683bc3491381f76908cacdc27e3b0c6091a7
DEVELoper <syabblogger@gmail.com>
2022-12-21 22:40:55 +0500

    changes in main activity of java file BTN added etc

--- app/src/main/java/com/example/conversionofipaddresstobinary/MainActivity.java
+++ app/src/main/java/com/example/conversionofipaddresstobinary/MainActivity.java
@@ -8,33 +8,95 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final int[] firstValueOfIP = new int[1];
-        final int[] secondValueOfIP = new int[1];
-        final int[] thirdValueOfIP = new int[1];
-        final int[] fourthValueOfIP = new int[1];
-        int[] defaultValues = {128, 64, 32, 16, 8, 4, 2, 1};
+         final int[] firstValueOfIP = new int[1];
+         final int[] secondValueOfIP = new int[1];
+         final int[] thirdValueOfIP = new int[1];
+         final int[] fourthValueOfIP = new int[1];
+         int[] defaultValues = {128, 64, 32, 16, 8, 4, 2, 1};
+         final int[] temp = new int[1];
+         final int[] binaryConvertedValue = new int[7];
 
         TextView firstValTextView = (TextView) findViewById(R.id.value1);
         TextView secondValTextView = (TextView) findViewById(R.id.value2);
         TextView thirdValTextView = (TextView) findViewById(R.id.value3);
         TextView fourthValTextView = (TextView) findViewById(R.id.value4);
+//        firstValTextView = Integer.parseInt(firstValTextView);
+//        secondValTextView = Integer.parseInt(secondValTextView);
+//        thirdValTextView = Integer.parseInt(thirdValTextView);
+//        fourthValTextView = Integer.parseInt(fourthValTextView);
         Button btn = (Button) findViewById(R.id.btn);
+        TextView result1 = (TextView) findViewById(R.id.resultFor1);
+
 
         btn.setOnClickListener(new View.OnClickListener(){
+            @Override
             public void onClick(View v)
             {
-                firstValueOfIP[0] = Integer.parseInt((String) firstValTextView.getText());
+                firstValueOfIP[0] = Integer.parseInt((String) firstValTextView.getText().toString());
 
+                int firstvalue = Integer.parseInt(String.valueOf(firstValueOfIP[0]));
                 if (firstValueOfIP[0] < defaultValues[0])
                 {
-
+                    temp[0] = firstValueOfIP[0]-defaultValues[0];
+                    binaryConvertedValue[0] = 1;
                 }
+                else if (firstValueOfIP[0] < defaultValues[1])
+                {
+                    binaryConvertedValue[0] = 0;
+                    temp[0] = firstValueOfIP[0]-defaultValues[1];
+                    binaryConvertedValue[1] = 1;
+                } else if(firstValueOfIP[0] < defaultValues[2])
+                {
+                    binaryConvertedValue[1] = 0;
+                    temp[0] = firstValueOfIP[0]-defaultValues[2];
+                    binaryConvertedValue[2] = 1;
+                }
+                else if(firstValueOfIP[0] < defaultValues[3])
+                {
+                    binaryConvertedValue[2] = 0;
+                    temp[0] = firstValueOfIP[0]-defaultValues[3];
+                    binaryConvertedValue[3] = 1;
+                }
+                else if(firstValueOfIP[0] < defaultValues[4])
+                {
+                    binaryConvertedValue[3] = 0;
+                    temp[0] = firstValueOfIP[0]-defaultValues[4];
+                    binaryConvertedValue[4] = 1;
+                }
+                else if(firstValueOfIP[0] < defaultValues[5])
+                {
+                    binaryConvertedValue[4] = 0;
+                    temp[0] = firstValueOfIP[0]-defaultValues[5];
+                    binaryConvertedValue[5] = 1;
+                }
+                else if(firstValueOfIP[0] < defaultValues[6])
+                {
+                    binaryConvertedValue[5] = 0;
+                    temp[0] = firstValueOfIP[0]-defaultValues[6];
+                    binaryConvertedValue[6] = 1;
+                }else if(firstValueOfIP[0] < defaultValues[7])
+                {
+                    binaryConvertedValue[6] = 0;
+                    temp[0] = firstValueOfIP[0]-defaultValues[7];
+                    binaryConvertedValue[7] = 1;
+                }
+
+
+                //// Display result of First Value of IP address
+
+                for (int j : binaryConvertedValue) {
+                    result1.setText(j);
+                }
+
+
 
                 secondValueOfIP[0] = Integer.parseInt((String) secondValTextView.getText());
                 thirdValueOfIP[0] = Integer.parseInt((String) thirdValTextView.getText());
